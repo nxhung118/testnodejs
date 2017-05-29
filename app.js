67412ffdfaacff3783c0ab1fbd1b4368ba848b48
nxhung118 <nxhung@csc.hcmus.edu.vn>
2017-05-29 16:49:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -73,4 +73,6 @@ server.on("request", function(req, res){
     }
 })
 
-server.listen("8080");
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
